4e39b4021d2f2431b9054e35bc190f60df8b7d76
shuchenliu <shuchenliu2014@gmail.com>
2017-10-16 22:07:52 -0700

    Completed coursework for Udacity Frontend Nanodegree

--- designs.js
+++ designs.js
@@ -2,9 +2,42 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+const submitButton = $(':submit');
 
-function makeGrid() {
+submitButton.on('click', (e) => {
+  e.preventDefault();
+  makeGrid(setListener);
+});
 
+
+function makeGrid(callback) {
 // Your code goes here!
+    const height = $('#input_height').val();
+    const width = $('#input_width').val();
 
+    let canvas = "<table id='gridTable'>";
+
+    for (let i = 0; i < height; ++i) {
+      let row = "<tr>";
+      for (let j = 0; j < width; ++j) {
+        row += "<td></td>";
+      }
+      row += "</tr>";
+      canvas += row;
+    }
+
+    if ($('#gridTable') !== null) {
+        $('#gridTable').remove();
+    }
+
+    $('#pixel_canvas').append(canvas);
+    callback();
+}
+
+const setListener = () => {
+  const canvas = $('#pixel_canvas');
+  canvas.on('click', 'td', (e) => {
+    const color = $('#colorPicker').val();
+    $(e.target).css('background-color', color);
+  });
 }
